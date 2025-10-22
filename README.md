13f5753a147741d8e7e7da4f35a70249e860fc6a
muralialakuntla3 <muralialakuntla3@gmail.com>
2025-10-22 18:31:20 +0530

    fixed bucket policy issues

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tf-s3-project
+terraform init
+terraform validate
+terraform plan
+terraform apply --var="aws-access-key=your_access_key" --var="aws-secret-key=your_secret"
--- modules/fl_s3_bucket/main.tf
+++ modules/fl_s3_bucket/main.tf
@@ -9,8 +9,24 @@ resource "aws_s3_bucket" "fl_bucket" {
   }
 }
 
+# Disable block public access for each bucket
+resource "aws_s3_bucket_public_access_block" "fl_bucket_block" {
+  count  = var.bucket_count
+  bucket = aws_s3_bucket.fl_bucket[count.index].id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Apply bucket policy after public access is unblocked
 resource "aws_s3_bucket_policy" "fl_bucket_policy" {
   count  = var.bucket_count
   bucket = aws_s3_bucket.fl_bucket[count.index].id
-  policy = file(var.policy_file)
+  policy = templatefile(var.policy_file, {
+    bucket_name = aws_s3_bucket.fl_bucket[count.index].bucket
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.fl_bucket_block]
 }
